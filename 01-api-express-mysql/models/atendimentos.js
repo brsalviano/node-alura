74dad7df7619ea461905840ea41f3b851bd3573d
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-08-09 01:40:18 -0300

    Api com express e mysql: Listagem, visualização, edição e exclusão de atendimentos

--- 01-api-express-mysql/models/atendimentos.js
+++ 01-api-express-mysql/models/atendimentos.js
@@ -42,10 +42,63 @@ class Atendimento {
         if (erro) {
           res.status(400).json(erro);
         } else {
-          res.status(201).json(resultados);
+          res.status(201).json(atendimento);
         }
       });
     }
+  }
+
+  lista(res) {
+    const sql = 'SELECT * from atendimentos';
+
+    conexao.query(sql, (erro, resultados) => {
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json(resultados);
+      }
+    });
+  }
+
+  buscaPorId(id, res) {
+    const sql = `SELECT * FROM atendimentos WHERE id=${id}`;
+
+    conexao.query(sql, (erro, resultados) => {
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json(resultados[0]);
+      }
+    });
+  }
+
+  altera(id, valores, res) {
+    if (valores.data) {
+      valores.data = moment(valores.data, 'DD/MM/YYYY').format(
+        'YYYY-MM-DD HH:mm:ss'
+      );
+    }
+    const sql = 'UPDATE atendimentos SET ? WHERE id=?';
+
+    conexao.query(sql, [valores, id], (erro, resultados) => {
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json({ ...valores, id });
+      }
+    });
+  }
+
+  deleta(id, res) {
+    const sql = 'DELETE FROM atendimentos WHERE id=?';
+
+    conexao.query(sql, id, (erro, resultados) => {
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json({ id });
+      }
+    });
   }
 }
 
